ebbca07d8ebbd45e4c404215f5e8b77d71e30158
Manish Jaiswal <106742042+jaiswalwrites@users.noreply.github.com>
2026-06-20 10:14:14 +0530

    feat: build socket listener execution thread

---
+++ sdk.py
@@ -0,0 +1,12 @@
+import socket
+import threading
+
+class InferenceNode:
+    def __init__(self, host: str, port: int):
+        self.host = host
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+if __name__ == "__main__":
+    pass
